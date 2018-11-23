b010b6fa11317fb19530eedb87f97d3f94b3e00b
div4bing <div4bing>
2018-11-22 20:52:05 -0500

    Final code

--- assign_5_part_a_6_2_optional.c
+++ assign_5_part_a_6_2_optional.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SOURCE_CITY 3             // NOTE: Modify this to have new source city
+#define DESTINATION_CITY 8        // NOTE: Modify this to have new destination city
+
 #define MAX_VERTEX 11
 #define INFI 9999
 #define ENABLE_DEBUG 0  // 0 to disable debugs, 1 to enable debugs
 
-struct City
+struct City             // Queue logic
 {
   int vertex;
   int distance;
   int root;
 };
 
-struct CityName
+struct CityName         // To store alpha name of city
 {
   char name;
 };
 
-int findMinQ(struct City cityQ[MAX_VERTEX]);
-int printQ(struct City cityQ[MAX_VERTEX]);
-void printPath(struct CityName cityname[MAX_VERTEX], int rootPath[], int dest, int invert);   // invert = 0 means Src to 0 and invert = 1 means 0 to src
+int findMinQ(struct City cityQ[MAX_VERTEX]);          // Finds current minimum distance from Q
+int printQ(struct City cityQ[MAX_VERTEX]);            // Print current Q
+void printCityPath(struct CityName cityname[MAX_VERTEX], int rootPath[], int dest, int invert);   // invert = 0 means Src to 0 and invert = 1 means 0 to src
 
 int findMinQ(struct City cityQ[MAX_VERTEX])
 {
@@ -27,11 +30,10 @@ int findMinQ(struct City cityQ[MAX_VERTEX])
   int min = INFI;
   int retCity = INFI;
 
-  for (i = 0; i < MAX_VERTEX * MAX_VERTEX; i++)
+  for (i = 0; i < MAX_VERTEX * MAX_VERTEX; i++)       // Check whole Q and ignore find min
   {
     if (cityQ[i].distance != INFI)
     {
-      // printf("min: %d & Dist- %d\n", min, cityQ[i].distance);
       if (min > cityQ[i].distance)
       {
         min = cityQ[i].distance;
@@ -40,7 +42,6 @@ int findMinQ(struct City cityQ[MAX_VERTEX])
     }
   }
 
-  // printf("Returning retCity from Q=%d\n", retCity);
   return retCity;
 }
 
@@ -49,7 +50,7 @@ int printQ(struct City cityQ[MAX_VERTEX])
   printf("City Q: [");
   for (int i = 0; i < MAX_VERTEX * MAX_VERTEX; i++)
   {
-    if (cityQ[i].distance != INFI)
+    if (cityQ[i].distance != INFI)                      // Print only valid entries
     {
       printf("(index=%d, vertex=%d, root=%d, dist=%d), ", i, cityQ[i].vertex, cityQ[i].root, cityQ[i].distance);
     }
@@ -59,9 +60,9 @@ int printQ(struct City cityQ[MAX_VERTEX])
   return 0;
 }
 
-void printPath(struct CityName cityname[MAX_VERTEX], int rootPath[], int dest, int invert)
+void printCityPath(struct CityName cityname[MAX_VERTEX], int rootPath[], int dest, int invert)
 {
-  if (invert == 0)    // From Src to 0
+  if (invert == 0)    // Prints From Src to 0
   {
     printf("%c->", cityname[dest].name);
     if (rootPath[dest] == 0)
@@ -70,9 +71,9 @@ void printPath(struct CityName cityname[MAX_VERTEX], int rootPath[], int dest, i
       return;
     }
 
-    printPath(&cityname[0], rootPath, rootPath[dest], invert);
+    printCityPath(&cityname[0], rootPath, rootPath[dest], invert);
   }
-  else                // From 0 to Src
+  else                // Prints From 0 to Src
   {
     if (rootPath[dest] == 0)
     {
@@ -80,7 +81,7 @@ void printPath(struct CityName cityname[MAX_VERTEX], int rootPath[], int dest, i
       return;
     }
 
-    printPath(&cityname[0], rootPath, rootPath[dest], invert);
+    printCityPath(&cityname[0], rootPath, rootPath[dest], invert);
     printf("->%c", cityname[dest].name);
   }
 }
@@ -103,10 +104,10 @@ int main (int argc, char *argv[])
   struct CityName cityname[MAX_VERTEX];
 
   int D[MAX_VERTEX] = {INFI};         // Resulting shortest dist a to each city, default all to infinite
-  struct City cityQ[MAX_VERTEX * MAX_VERTEX];
+  struct City cityQ[MAX_VERTEX * MAX_VERTEX]; // Q
   int i = 0, j = 0, k = 0, z = 0;
   int qCity=0;
-  int rootPath[MAX_VERTEX] = {INFI};
+  int rootPath[MAX_VERTEX] = {INFI};  // stores Path of parent
   int finalizedCity[MAX_VERTEX];      // 0 means pending and 1 means finalized distance
 
   for (i = 0; i < MAX_VERTEX * MAX_VERTEX; i++)   // Defult everything to INFI
@@ -116,60 +117,58 @@ int main (int argc, char *argv[])
     cityQ[i].root = INFI;
   }
 
-  for (i = 0; i < MAX_VERTEX; i++)   // Defult everything to INFI
+  for (i = 0; i < MAX_VERTEX; i++)   // Defulting
   {
     D[i] = INFI;
     finalizedCity[i] = 0;
     cityname[i].name = (char)(0x61 + (char)i);      // Load alpha names of city
   }
 
-  D[0] = 0;       // City a's distance to itself is 0
-  finalizedCity[0] = 1; // City a is finalized already
-  rootPath[0] = 0;
-  k = 0;          // Our Q
+  D[0] = 0;               // City a's distance to itself is 0
+  finalizedCity[0] = 1;   // City a is finalized already
+  rootPath[0] = 0;        // City a's parent is itself
+  k = 0;                  // Our Q
 
   i = 0;
-  while (1)           // Untill we add all the vertex
+  while (1)           // Untill Q is empty
   {
-    // find adjecent
+    // find adjecent and add them to Q
     for (j = 0; j < MAX_VERTEX; j++)
     {
-      // printf("Adding: finalizedCity[%d]=%d\n", j, finalizedCity[j]);
       if ((graph[i][j] != INFI) && (i != j) && (j != 0) && (finalizedCity[j] != 1))
       {
         cityQ[k].vertex = j;
         cityQ[k].distance = graph[i][j];
         cityQ[k++].root = i;
-        // printf("\n--->Added to Q Root=%d Vertex=%d Dis=%d\n", i, j, graph[i][j]);
       }
     }
-    // printQ(&cityQ[0]);  // Print current Q
+#if ENABLE_DEBUG
+    printQ(&cityQ[0]);                // Print current Q
+#endif
 
     qCity = findMinQ(&cityQ[0]);      // Vertex who has min distance in adjecent
 
-    if (qCity == INFI)  // PQ empty now
+    if (qCity == INFI)                // Q empty now
     {
       break;
     }
 
-    // printf("Working on Vertex: %d\n", cityQ[qCity].vertex);
-    // printf("D[%d]=> Current Dist: %d, Possible distance:%d\n", cityQ[qCity].vertex, D[cityQ[qCity].vertex], (D[cityQ[qCity].root] + graph[cityQ[qCity].root][cityQ[qCity].vertex]));
-    if (D[cityQ[qCity].vertex] > (D[cityQ[qCity].root] + graph[cityQ[qCity].root][cityQ[qCity].vertex]))
+    if (D[cityQ[qCity].vertex] > (D[cityQ[qCity].root] + graph[cityQ[qCity].root][cityQ[qCity].vertex]))    // If new distance is smaller, update it
     {
       D[cityQ[qCity].vertex] = D[cityQ[qCity].root] + graph[cityQ[qCity].root][cityQ[qCity].vertex];
       rootPath[cityQ[qCity].vertex] = cityQ[qCity].root;
       finalizedCity[cityQ[qCity].vertex] = 1;   // City's shortest distance finalized
     }
 
-    i = cityQ[qCity].vertex;
+    i = cityQ[qCity].vertex;          // Store it and go for next adjecent
 
-    // printf("DONE: i=%d D[i]=%d rootPath[i]=%d\n", cityQ[qCity].vertex, D[cityQ[qCity].vertex], rootPath[cityQ[qCity].vertex]);
-    cityQ[qCity].vertex = INFI;       // Reset the one we know is done
+    cityQ[qCity].vertex = INFI;       // Reset the one we know is done in Q
     cityQ[qCity].distance = INFI;
     cityQ[qCity].root = INFI;
     z++;
   }
 
+#if ENABLE_DEBUG
   printf("PATH:\n");
   for (i = 0; i < MAX_VERTEX; i++)
   {
@@ -182,13 +181,14 @@ int main (int argc, char *argv[])
     printf("*************************************\n");
     printf("Min distance- %d - %d\n", i, D[i]);
     printf("Path of %d: \n", i);
-    printPath(&cityname[0], rootPath, i, 1);
+    printCityPath(&cityname[0], rootPath, i, 1);
     printf("\n");
   }
+#endif
 
-  printf("Shortest Path from d to i via a is: ");
-  printPath(&cityname[0], rootPath, 3, 0);
-  printPath(&cityname[0], rootPath, 8, 1);
+  printf("Shortest Path from d to i via a is: ");               // Print result path
+  printCityPath(&cityname[0], rootPath, SOURCE_CITY, 0);                  // NOTE: Modify source city in macros to check other city
+  printCityPath(&cityname[0], rootPath, DESTINATION_CITY, 1);             // NOTE: Modify destination city in macros to check other city
   printf("\n");
 
   return 0;
